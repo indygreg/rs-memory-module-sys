09cce22a0fda895f021a354671e928dc8275cdfc
Yin Guanhao <sopium@mysterious.site>
2020-04-19 09:22:57 +0800

    Workaround docs.rs build failure

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 fn main() {
-    println!("cargo:rerun-if-changed=src/memorymodule.c");
-    println!("cargo:rerun-if-changed=src/memorymodule.h");
+    // Skip C compilation if building on docs.rs.
+    if cfg!(not(memory_module_sys_docs_rs)) {
+        println!("cargo:rerun-if-changed=src/memorymodule.c");
+        println!("cargo:rerun-if-changed=src/memorymodule.h");
 
-    cc::Build::new()
-        .file("src/memorymodule.c")
-        .compile("memorymodule");
+        cc::Build::new()
+            .file("src/memorymodule.c")
+            .compile("memorymodule");
+    }
 }
